2f189e0006f533f2cf01a1f588fdb3ade8566e73
Sergey Taranov <s.taranov@treatstock.com>
2021-05-18 01:53:55 +0300

    Controller and start script added

---
+++ README.md
@@ -0,0 +1,13 @@
+# DigiByte payment verification system using NOWNodes
+This is an example payment verification system using node.js and NOWNodes for the blockchain interface
+
+You can contact NOWNodes and request a free personal API key to begin with, by visiting https://nownodes.io/pricing and clicking "Order now". You will need this API key to interface with their service.
+
+You can then define how much you expect a user to pay for your service, for example 100 DGB (Or you can extend upon this library and make it dynamic, for example if you were taking payments for varying goods and services), and then once it notices the payment has been made it will validate and permit the user to proceed to the next step in the process.
+
+## Configuration
+
+1. Edit .env.example and put your NOWNodes API key in there, save it as .env
+2. Modify the example.js
+3. Run <pre>npm install</p>
+4. Run <pre>npm start</p>
---
+++ start.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const { Controller } = require('./controller.js');
+
+const server = http.createServer((request, response) => {
+    const { headers, method, url } = request;
+
+    // Website you wish to allow to connect
+    response.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+
+    // Request headers you wish to allow
+    // response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    // response.setHeader('Access-Control-Allow-Credentials', true);
+
+    let responseEnd = (el) => {
+        console.log('=>', el)
+        response.end(el)
+    }
+
+    let body = [];
+    request
+        .on('error', (err) => { console.error(err) })
+        .on('data', (chunk) => { body.push(chunk) })
+        .on('end', async () => {
+            body = Buffer.concat(body).toString()
+            const responseBody = { headers, method, url, body }
+
+            response.on('error', (err) => {
+                console.error(err);
+            });
+
+            response.writeHead(200, {'Content-Type': 'application/json'})
+
+            let controller = new Controller()
+            await controller.run(
+                responseBody,
+                (result) => {
+                    console.log('!!', result)
+                    responseEnd(JSON.stringify(result))
+                }
+            )
+        });
+})
+
+server.listen(60001);
